a6b9188fd1947ded83c0063f7db0037b7ff65876
drull1000 <pcaetanop@protonmail.com>
2021-06-28 18:57:57 -0300

    Profile

---
+++ src/components/Profile/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { View, Text, Alert } from "react-native";
+import { styles } from "./styles";
+
+export function Profile() {
+  return (
+    <View style={styles.container}>
+      <View>
+        <View style={styles.user}>
+          <Text style={styles.greeting}>Olá</Text>
+          <Text style={styles.username}>Drull</Text>
+        </View>
+
+        <Text style={styles.message}>Hoje é dia de vitória</Text>
+      </View>
+    </View>
+  );
+}
