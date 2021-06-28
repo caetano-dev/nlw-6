3a368ee378f35329df312ba9e00bd219761dbb63
drull1000 <pcaetanop@protonmail.com>
2021-06-28 18:58:13 -0300

    SignIn

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { View, Image, Text } from "react-native";
+import { styles } from "./styles";
+import { ButtonIcon } from "../../components/ButtonIcon";
+import IllustrationImg from "../../assets/illustration.png";
+export function SignIn() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Conecte-se {"\n"}e organize suas {"\n"}
+          jogatinas
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Crie grupos para jogar seus games {"\n"}
+          favoritos com seus amigos
+        </Text>
+        <ButtonIcon title="Entrar com discord" />
+      </View>
+    </View>
+  );
+}
