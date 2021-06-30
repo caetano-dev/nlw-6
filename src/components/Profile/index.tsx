14643137033d55627d9899b51bca134e9aa15f69
drull1000 <pcaetanop@protonmail.com>
2021-06-30 17:32:58 -0300

    Adding avatar

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text, Alert } from "react-native";
+import { Avatar } from "../Avatar";
 import { styles } from "./styles";
 
 export function Profile() {
   return (
     <View style={styles.container}>
+      <Avatar urlImage="https://github.com/drull1000.png" />
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá</Text>
