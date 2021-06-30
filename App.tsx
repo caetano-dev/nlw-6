b2c26e161a54ee523d27f7f134a889b9e99602d0
drull1000 <pcaetanop@protonmail.com>
2021-06-30 17:31:16 -0300

    Adding route

--- App.tsx
+++ App.tsx
@@ -6,7 +6,7 @@ import {
   Rajdhani_700Bold,
 } from "@expo-google-fonts/rajdhani";
 import AppLoading from "expo-app-loading";
-import { Home } from "./src/screens/Home";
+import { Routes } from "./src/routes";
 import { StatusBar } from "react-native";
 import { Background } from "./src/components/Background";
 
@@ -27,7 +27,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      <Home />
+      <Routes />
     </Background>
   );
 }
