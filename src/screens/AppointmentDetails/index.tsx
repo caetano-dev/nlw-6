cba294e6237d9512c5c68d643e60d83dd95bea71
drull1000 <pcaetanop@protonmail.com>
2021-07-03 13:23:33 -0300

    Adding members

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,10 +1,33 @@
 import React from "react";
 import { Fontisto } from "@expo/vector-icons";
 import { Background } from "../../components/Background";
+import { ListHeader } from "../../components/ListHeader";
+import { ButtonIcon } from "../../components/ButtonIcon";
+
 import { Header } from "../../components/Header";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { theme } from "../../global/styles/theme";
+import { ImageBackground, Text, View, FlatList } from "react-native";
+import BannerImg from "../../assets/banner.png";
+import { styles } from "./styles";
+import { Member } from "../../components/Member";
+import { ListDivider } from "../../components/ListDivider";
+
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: "1",
+      username: "Drull",
+      avatar_url: "https://github.com/drull1000.png",
+      status: "online",
+    },
+    {
+      id: "2",
+      username: "Drull1000",
+      avatar_url: "https://github.com/drull1000.png",
+      status: "offline",
+    },
+  ];
   return (
     <Background>
       <Header
@@ -15,6 +38,25 @@ export function AppointmentDetails() {
           </BorderlessButton>
         }
       />
+      <ImageBackground source={BannerImg} style={styles.banner}>
+        <View style={styles.bannerContent}>
+          <Text style={styles.title}>Lendários</Text>
+          <Text style={styles.subtitle}>
+            É hoje que vamos chegar ao challenger sem perder uma partida da md10
+          </Text>
+        </View>
+      </ImageBackground>
+      <ListHeader title="Jogadores" subtitle="Total 3" />
+      <FlatList
+        data={members}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Member data={item} />}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.members}
+      />
+      <View style={styles.footer}>
+        <ButtonIcon title="Entrar na partida" />
+      </View>
     </Background>
   );
 }
