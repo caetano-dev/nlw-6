434ad1619fa4d677e196c064f887d3605ca1f9d2
drull1000 <pcaetanop@protonmail.com>
2021-07-04 11:37:17 -0300

    Adding more fields to the form

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -4,47 +4,93 @@ import { CategorySelect } from "../../components/CategorySelect";
 import { RectButton } from "react-native-gesture-handler";
 import { Header } from "../../components/Header";
 import { GuildIcon } from "../../components/GuildIcon";
-
 import { Feather } from "@expo/vector-icons";
-import { Text, View } from "react-native";
+import {
+  Text,
+  View,
+  ScrollView,
+  Platform,
+  KeyboardAvoidingView,
+} from "react-native";
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
+import { SmallInput } from "../../components/SmallInput";
+import { TextArea } from "../../components/TextArea";
+import { Button } from "../../components/Button";
 
 export function AppointmentCreate() {
   const [category, setCategory] = useState("");
 
   return (
-    <Background>
-      <Header title="Agendar partida" />
-      <Text
-        style={[
-          styles.label,
-          { marginLeft: 24, marginTop: 25, marginBottom: 18 },
-        ]}
-      >
-        Categoria
-      </Text>
-      <CategorySelect
-        hasCheckBox
-        setCategory={setCategory}
-        categorySelected={category}
-      />
-      <View style={styles.form}>
-        <RectButton>
-          <View style={styles.select}>
-            {/*  <View style={styles.image}/>*/ <GuildIcon />}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.container}
+    >
+      <ScrollView>
+        <Background>
+          <Header title="Agendar partida" />
+          <Text
+            style={[
+              styles.label,
+              { marginLeft: 24, marginTop: 25, marginBottom: 18 },
+            ]}
+          >
+            Categoria
+          </Text>
+          <CategorySelect
+            hasCheckBox
+            setCategory={setCategory}
+            categorySelected={category}
+          />
+          <View style={styles.form}>
+            <RectButton>
+              <View style={styles.select}>
+                {/*  <View style={styles.image}/>*/ <GuildIcon />}
 
-            <View style={styles.selectBody}>
-              <Text style={styles.label}>Selecione um servidor</Text>
+                <View style={styles.selectBody}>
+                  <Text style={styles.label}>Selecione um servidor</Text>
+                </View>
+                <Feather
+                  name="chevron-right"
+                  color={theme.colors.heading}
+                  size={18}
+                />
+              </View>
+            </RectButton>
+            <View style={styles.field}>
+              <View>
+                <Text style={styles.label}>Dia e mês</Text>
+                <View style={styles.column}>
+                  <SmallInput maxLength={2} />
+                  <Text style={styles.divider}>/</Text>
+                  <SmallInput maxLength={2} />
+                </View>
+              </View>
+              <View>
+                <Text style={styles.label}>Hora e minuto</Text>
+                <View style={styles.column}>
+                  <SmallInput maxLength={2} />
+                  <Text style={styles.divider}>:</Text>
+                  <SmallInput maxLength={2} />
+                </View>
+              </View>
             </View>
-            <Feather
-              name="chevron-right"
-              color={theme.colors.heading}
-              size={18}
+            <View style={[styles.field, { marginBottom: 12 }]}>
+              <Text style={styles.label}>Descrição</Text>
+            </View>
+            <Text style={styles.caracteresLimit}>Max 100 caracteres</Text>
+            <TextArea
+              multiline
+              maxLength={100}
+              numberOfLines={5}
+              autoCorrect={false}
             />
+            <View style={styles.footer}>
+              <Button title="Agendar" />
+            </View>
           </View>
-        </RectButton>
-      </View>
-    </Background>
+        </Background>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
