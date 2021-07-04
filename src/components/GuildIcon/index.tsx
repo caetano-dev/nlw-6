4377c94ca1d49621033b70138d557c1375455ec6
drull1000 <pcaetanop@protonmail.com>
2021-07-04 15:58:20 -0300

    New icon

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { Image } from "react-native";
 import { styles } from "./styles";
 
-const uri = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn.iconscout.com%2Ficon%2Ffree%2Fpng-256%2Fdiscord-3-569463.png&f=1&nofb=1'
+const uri =
+  "https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.net-aware.org.uk%2Fsiteassets%2Fimages-and-icons%2Fapplication-icons%2Fapp-icons-discord.png%3Fw%3D585%26scale%3Ddown&f=1&nofb=1";
 
 export function GuildIcon() {
   return <Image source={{ uri }} style={styles.image} resizeMode="cover" />;
