e6596942c0d1bf39fdecc8faf48c2604d9f8abe6
drull1000 <pcaetanop@protonmail.com>
2021-07-05 10:41:14 -0300

    New styles

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -1,14 +1,11 @@
-import { StyleSheet } from "react-native";
-import { theme } from "../../global/styles/theme";
+import { StyleSheet } from 'react-native';
+import { theme } from '../../global/styles/theme';
 
 export const styles = StyleSheet.create({
   container: {
     height: 1,
-    width: "75%",
-    alignSelf: "flex-end",
-    backgroundColor: theme.colors.secondary40,
-    marginVertical: 31,
-    marginTop: 2,
-
-  },
+    width: '78%',
+    alignSelf: 'flex-end',
+    backgroundColor: theme.colors.secondary40,    
+  }
 });
