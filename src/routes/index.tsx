66d6468fa0760d397928a7b744b4594045a2146a
drull1000 <pcaetanop@protonmail.com>
2021-07-05 17:23:46 -0300

    Organizing imports

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
+import React from "react";
 
 export function Routes() {
   return (
