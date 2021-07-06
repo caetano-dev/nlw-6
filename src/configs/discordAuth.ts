b54e98c0a515ac82d7240cbb17cf1d76a1dd2ebf
drull1000 <pcaetanop@protonmail.com>
2021-07-06 20:00:41 -0300

    Auth variables

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,7 @@
+const REDIRECT_URI = "https://auth.expo.io/@anonymous/gameplay-29c3d380-6abd-4262-9437-fd161893f30e";
+const SCOPE = "identify%20email%20connections%20guilds";
+const RESPONSE_TYPE = "token";
+const CLIENT_ID = "861948618578722826";
+const CDN_IMAGE = "https://cdn.discordapp.com";
+
+export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
